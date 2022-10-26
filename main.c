768f68f676d1849ac46dba0111439c7129e4d2e3
MaximeJanez <maxime.janez@hotmail.com>
2022-10-26 16:11:22 +0200

    commentaire add

--- main.c
+++ main.c
@@ -6,14 +6,14 @@ int main()
     // déclaration des différentes variables
     //****************************************************************************************************
 
-    int nb_allumettes;
+    int nb_allumettes, save_nb_allumettes;
     int nb_allumettes_prises;
     int joueur = 1;
     int fin = 0;
-    int regle = 0 ;
-    int nb_joueur;
+    int regle = 0 , save_regle;
+    int nb_joueur, save_nb_joueur;
     int rejouer = 1;
-    int Config_garder;
+    int config_garder = 0;
     char nom_joueur[20];
 
     //****************************************************************************************************
@@ -34,6 +34,9 @@ int main()
     printf("Votre choix : ");
     scanf("%d", &regle);
 
+    //****************************************************************************************************
+    // vérification de la saisie de l'utilisateur pour les règles
+    //****************************************************************************************************
 
     while (regle != 1 && regle != 2)
     {
@@ -50,7 +53,7 @@ int main()
     {
         printf("Le jeu se joue a deux joueurs ou contre un ordinateur.\n");
         printf("Au debut de la partie, il y a un nombre allumettes que vous aller choisir.\n");
-        printf("A tour de role, les joueurs enlèvent 1, 2 ou 3 allumettes.\n");
+        printf("A tour de role, les joueurs enlevent 1, 2 ou 3 allumettes.\n");
         printf("Celui qui prend la derniere allumette a perdu.\n\n");
     }
 
@@ -58,20 +61,18 @@ int main()
     // configuration des paramètres de la partie (intervient au d"but et pour rejouer une partie)
     //************************************************************************************************
 
-    if(Config_garder ==1 )
+    if(config_garder != 1 )
     {
-        printf("Vous avez choisi de garder les regles par defaut.\n\n");
-    }
-    else
-    {
-        printf("Avant de commencer la partie nous allons configurer les différents paramètres.\n");
+        printf("Avant de commencer la partie nous allons configurer les differents parametres.\n");
         printf("Combien de joueur vont jouer cette partie ?\n");
         printf("Saisir le nombre de joueur : \n");
         scanf("%d", &nb_joueur);
 
+        //****************************************************************************************************
+        // vérification de la saisie de l'utilisateur pour le nombre de joueur
+        //****************************************************************************************************
 
-
-        if (nb_joueur != 1 || nb_joueur != 2)
+        while (nb_joueur != 1 && nb_joueur != 2)
         {
             printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
             printf("Saisir le nombre de joueur : \n");
@@ -86,7 +87,7 @@ int main()
         // vérification de la saisie de l'utilisateur du nombre d'allumettes
         //****************************************************************************************************
 
-        while (nb_allumettes < 10 || nb_allumettes > 100)
+        while (nb_allumettes <= 9 || nb_allumettes >=101)
         {
             printf("Vous avez saisi un nombre d'allumettes inferieur a 10 ou superieur a 100.\n");
             printf("Veuillez resaisir le nombre d'allumettes : \n");
@@ -101,6 +102,13 @@ int main()
 
     while (rejouer == 1) // variable a 1 quand une partie ce lance
     {
+        //****************************************************************************************************
+        // save des paramètres de la partie
+        //****************************************************************************************************
+
+        save_nb_allumettes = nb_allumettes;
+        save_nb_joueur = nb_joueur;
+        save_regle = regle;
 
         while (fin == 0) // temps que la parti n'est pas fini
         {
@@ -138,41 +146,67 @@ int main()
             fin = 1;
         }
 
+
 //****************************************************************************************************
 // demande de rejouer une partie
 // ****************************************************************************************************
+
         printf("Voulez vous rejouer ?\n");
         printf("1. Oui\n");
         printf("2. Non\n");
         printf("Votre choix : ");
         scanf("%d", &rejouer);
-        if (rejouer != 1 || rejouer != 2)
+
+        //****************************************************************************************************
+        // vérification de la saisie de l'utilisateur pour rejouer une partie
+        //****************************************************************************************************
+
+        while  (rejouer != 1 && rejouer != 2)
         {
+            printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
+            printf("Votre choix : ");
+            scanf("%d", &rejouer);
+        }
 
-            //****************************************************************************************************
-            // vérification de la saisie de l'utilisateur pour rejouer une partie
-            //****************************************************************************************************
+        if (rejouer == 1)
+        {
+            printf("Vous avez choisi de rejouer.\n\n");
+            fin = 0;
+            nb_allumettes = save_nb_allumettes;
+            nb_joueur = save_nb_joueur;
+            regle = save_regle;
 
-            while (rejouer != 1 && rejouer != 2)
+
+            printf("Voulez vous conserver les meme parametre que la derniere partie ?\n");
+            printf("1. Oui\n");
+            printf("2. Non\n");
+            printf("Votre choix : ");
+            scanf("%d", &config_garder);
+
+            while (config_garder != 1 && config_garder != 2)
             {
                 printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
-                scanf("%d", &rejouer);
+                printf("Votre saisi : ");
+                scanf("%d", &config_garder);
             }
-            if (rejouer == 1)
+            if (config_garder == 1)
             {
-                printf("Vous avez choisi de rejouer.\n\n");
-                printf("Voulez vous conserver les meme parametre que la derniere partie ?\n");
-                printf("1. Oui\n");
-                printf("2. Non\n");
-                printf("Votre choix : ");
-                scanf("%d", &Config_garder);
+                printf("Vous avez choisi de conserver les meme parametre que la derniere partie.\n\n");
                 printf("Voter choix a bien ete pris en compte.\n\n");
             }
-            if (rejouer == 2)
+            else
             {
-                printf("Au revoir et a bientot !\n");
-
+                printf("Vous avez choisi de changer les parametre de la partie.\n\n");
+                printf("Saisir le nombre de joueur : \n");
+                scanf("%d", &nb_joueur);
+                printf("Saisir le nombre d'allumettes : \n");
+                scanf("%d", &nb_allumettes);
             }
+        }
+        else
+        {
+            printf("Vous avez choisi de ne pas rejouer.\n\n");
+            printf("Merci d'avoir jouer et a bientot.\n\n");
         }
     }
 
