1120fb3c616d754c30d772d4a66894239469a77c
chaima <chaimagamez@gmail.com>
2022-11-10 20:02:25 +0100

    création des procédures

--- main.c
+++ main.c
@@ -1,4 +1,113 @@
 # include <stdio.h>
+# include <stdlib.h>
+
+void verification_utilisateur(int regle){
+    while (regle != 1 && regle != 2)
+    {
+        printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
+        printf("Votre saisi : ");
+        scanf("%d", &regle);
+    }
+}
+
+
+void demande_regle(int regle){
+    if (regle == 1) {
+        printf("Le jeu se joue a deux joueurs ou contre un ordinateur.\n");
+        printf("Au debut de la partie, il y a un nombre allumettes que vous aller choisir.\n");
+        printf("A tour de role, les joueurs enlèvent 1, 2 ou 3 allumettes.\n");
+        printf("Celui qui prend la derniere allumette a perdu.\n\n");
+    }
+    if (regle == 1)
+    {
+        printf("Le jeu se joue a deux joueurs ou contre un ordinateur.\n");
+        printf("Au debut de la partie, il y a un nombre allumettes que vous aller choisir.\n");
+        printf("A tour de role, les joueurs enlevent 1, 2 ou 3 allumettes.\n");
+        printf("Celui qui prend la derniere allumette a perdu.\n\n");
+    }
+}
+
+void verification_joueur(int nb_joueur){
+    while (nb_joueur != 1 && nb_joueur != 2)
+    {
+        printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
+        printf("Saisir le nombre de joueur : \n");
+        scanf("%d", &nb_joueur);
+    }
+}
+
+void verification_allumette(int nb_allumettes){
+    while (nb_allumettes <= 9 || nb_allumettes >=101)
+    {
+        printf("Vous avez saisi un nombre d'allumettes inferieur a 10 ou superieur a 100.\n");
+        printf("Veuillez resaisir le nombre d'allumettes : \n");
+        scanf("%d", &nb_allumettes);
+    }
+}
+
+void changement_joueur(int joueur){
+    if (joueur == 1)
+    {
+        joueur = 2;
+    }
+    else
+    {
+        joueur = 1;
+    }
+}
+
+void verification_rejouer(int rejouer, int config_garder, int nb_joueur, int nb_allumettes){
+    int fin;
+    int save_nb_joueur;
+    int save_nb_allumettes;
+    int save_regle;
+
+    while  (rejouer != 1 && rejouer != 2)
+    {
+        printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
+        printf("Votre choix : ");
+        scanf("%d", &rejouer);
+    }
+
+    if (rejouer == 1){
+        printf("Vous avez choisi de rejouer.\n\n");
+        fin = 0;
+        nb_allumettes = save_nb_allumettes;
+        nb_joueur = save_nb_joueur;
+        //regle = save_regle;
+        printf("Voulez vous conserver les meme parametre que la derniere partie ?\n");
+        printf("1. Oui\n");
+        printf("2. Non\n");
+        printf("Votre choix : ");
+        scanf("%d", &config_garder);
+
+        while (config_garder != 1 && config_garder != 2)
+        {
+            printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
+            printf("Votre saisi : ");
+            scanf("%d", &config_garder);
+        }
+
+        if (config_garder == 1)
+        {
+            printf("Vous avez choisi de conserver les meme parametre que la derniere partie.\n\n");
+            printf("Voter choix a bien ete pris en compte.\n\n");
+        }
+        else
+        {
+            printf("Vous avez choisi de changer les parametre de la partie.\n\n");
+            printf("Saisir le nombre de joueur : \n");
+            scanf("%d", &nb_joueur);
+            printf("Saisir le nombre d'allumettes : \n");
+            scanf("%d", &nb_allumettes);
+        }
+    }
+    else
+    {
+        printf("Vous avez choisi de ne pas rejouer.\n\n");
+        printf("Merci d'avoir jouer et a bientot.\n\n");
+    }
+}
 
 int main()
 {
@@ -33,38 +142,20 @@ int main()
     //****************************************************************************************************
     // vérification de la saisie de l'utilisateur pour les règles
     //****************************************************************************************************
+    verification_utilisateur(regle);
 
-    while (regle != 1 && regle != 2)
-    {
-        printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
-        printf("Votre saisi : ");
-        scanf("%d", &regle);
-    }
     //****************************************************************************************************
     // fonction affichage du prenom du joueur
     //****************************************************************************************************
 
-        printf("Entrer votre prénom: ");
-        scanf("%s", &nom_joueur);
-        printf("Bonjour %s !\n", nom_joueur);
+    printf("Entrer votre prénom: ");
+    scanf("%s", &nom_joueur);
+    printf("Bonjour %s !\n", nom_joueur);
 
     //****************************************************************************************************
     // fonction affichage des règles si demander par l'utilisateur
     //****************************************************************************************************
-
-        if (regle == 1) {
-            printf("Le jeu se joue a deux joueurs ou contre un ordinateur.\n");
-            printf("Au debut de la partie, il y a un nombre allumettes que vous aller choisir.\n");
-            printf("A tour de role, les joueurs enlèvent 1, 2 ou 3 allumettes.\n");
-            printf("Celui qui prend la derniere allumette a perdu.\n\n");
-        }
-    if (regle == 1)
-    {
-        printf("Le jeu se joue a deux joueurs ou contre un ordinateur.\n");
-        printf("Au debut de la partie, il y a un nombre allumettes que vous aller choisir.\n");
-        printf("A tour de role, les joueurs enlevent 1, 2 ou 3 allumettes.\n");
-        printf("Celui qui prend la derniere allumette a perdu.\n\n");
-    }
+    demande_regle(regle);
 
     //************************************************************************************************
     // configuration des paramètres de la partie (intervient au d"but et pour rejouer une partie)
@@ -80,13 +171,8 @@ int main()
         //****************************************************************************************************
         // vérification de la saisie de l'utilisateur pour le nombre de joueur
         //****************************************************************************************************
+        verification_joueur(nb_joueur);
 
-        while (nb_joueur != 1 && nb_joueur != 2)
-        {
-            printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
-            printf("Saisir le nombre de joueur : \n");
-            scanf("%d", &nb_joueur);
-        }
         printf("Maintenant, combien d'allumettes voulez vous mettre au debut de la partie ?\n");
         printf("Votre choix doit etre compris entre 10 et 100 \n");
         printf("Saisir le nombre d'allumettes : \n");
@@ -95,13 +181,7 @@ int main()
         //****************************************************************************************************
         // vérification de la saisie de l'utilisateur du nombre d'allumettes
         //****************************************************************************************************
-
-        while (nb_allumettes <= 9 || nb_allumettes >=101)
-        {
-            printf("Vous avez saisi un nombre d'allumettes inferieur a 10 ou superieur a 100.\n");
-            printf("Veuillez resaisir le nombre d'allumettes : \n");
-            scanf("%d", &nb_allumettes);
-        }
+        verification_allumette(nb_allumettes);
         printf("Vous avez choisi de jouer a %d joueur(s) et de mettre %d allumettes au debut de la partie.\n", nb_joueur, nb_allumettes);
     }
 
@@ -141,14 +221,7 @@ int main()
                     //****************************************************************************************************
                     // changement de joueur
                     //****************************************************************************************************
-                    if (joueur == 1)
-                    {
-                        joueur = 2;
-                    }
-                    else
-                    {
-                        joueur = 1;
-                    }
+                    changement_joueur(joueur);
                 }
             }
             printf("Le joueur %d gagne !\n", joueur);
@@ -170,55 +243,7 @@ int main()
         // vérification de la saisie de l'utilisateur pour rejouer une partie
         //****************************************************************************************************
 
-        while  (rejouer != 1 && rejouer != 2)
-        {
-            printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
-            printf("Votre choix : ");
-            scanf("%d", &rejouer);
-        }
-
-        if (rejouer == 1)
-        {
-            printf("Vous avez choisi de rejouer.\n\n");
-            fin = 0;
-            nb_allumettes = save_nb_allumettes;
-            nb_joueur = save_nb_joueur;
-            regle = save_regle;
-
-
-            printf("Voulez vous conserver les meme parametre que la derniere partie ?\n");
-            printf("1. Oui\n");
-            printf("2. Non\n");
-            printf("Votre choix : ");
-            scanf("%d", &config_garder);
-
-            while (config_garder != 1 && config_garder != 2)
-            {
-                printf("Vous avez saisi autre chose que 1 ou 2 donc veillez resaissir votre choix\n");
-                printf("Votre saisi : ");
-                scanf("%d", &config_garder);
-            }
-            if (config_garder == 1)
-            {
-                printf("Vous avez choisi de conserver les meme parametre que la derniere partie.\n\n");
-                printf("Voter choix a bien ete pris en compte.\n\n");
-            }
-            else
-            {
-                printf("Vous avez choisi de changer les parametre de la partie.\n\n");
-                printf("Saisir le nombre de joueur : \n");
-                scanf("%d", &nb_joueur);
-                printf("Saisir le nombre d'allumettes : \n");
-                scanf("%d", &nb_allumettes);
-            }
-        }
-        else
-        {
-            printf("Vous avez choisi de ne pas rejouer.\n\n");
-            printf("Merci d'avoir jouer et a bientot.\n\n");
-        }
+        verification_rejouer(rejouer, config_garder, nb_joueur, nb_allumettes);
     }
 
     return 0;
-}
-
